d0f8501eef7d0742c884d67a87ad6a387c8361be
T.Kav <tomas@plienas.lt>
2022-01-26 16:07:58 +0200

    uzbaigta uzduotis laipsniu konverteris, 2022.01.26

--- fahrenheit.js
+++ fahrenheit.js
@@ -1,62 +1,34 @@
-// function SubstractValues() {
-//     const first = getElementNumberValue("first")
-//         // console.log(first)
-//     const second = getElementNumberValue("second")
-//         // console.log(second)
-//     const SubstractValues = first - second
-//     writeResult(SubstractValues)
-// }
-function getElementNumberValue(id) {
+// Veikiantis konverteris Celsius/Fahrenheit/Celsius
+
+function getElementValue(id) {
     const firstElement = document.getElementById(id)
     const value = Number(firstElement.value)
     return value
 }
 
-
-
 function convert() {
-    const celsius = getElementNumberValue("first")
-        // console.log(celsius)
-    const fahrenheit = getElementNumberValue("second")
-        // console.log(fahrenheit)
-    if (celsius > 0 && fahrenheit === 0) {
+    const celsius = getElementValue("first")
+    const fahrenheit = getElementValue("second")
+    if (typeof celsius === 'number' && fahrenheit === 0) {
         const convert = celsius * 1.8 + 32;
         calculatedResult(convert)
-    } else if (fahrenheit > 0 && celsius === null) {
+    } else if (typeof fahrenheit === 'number' && celsius === 0) {
         const convert = (fahrenheit - 32) * 5 / 9;
         calculatedResult(convert)
     } else {
-        const convert = "netinkamos reiksmes"
-        calculatedResult(convert)
+        calculatedResult()
     }
 }
 
 function calculatedResult(result) {
+    const celsius = getElementValue("first")
+    const fahrenheit = getElementValue("second")
     const resultParagraph = document.getElementById("result")
-    resultParagraph.innerHTML = "result is: " + result
+    if (fahrenheit === 0) {
+        resultParagraph.innerHTML = "Atsakymas: " + result.toFixed(2) + " " + "F°"
+    } else if (celsius === 0) {
+        resultParagraph.innerHTML = "Atsakymas: " + result.toFixed(2) + " " + "C°"
+    } else {
+        resultParagraph.innerHTML = 'Atsakymas: klaida, patikrinkite sąlygas.'
+    }
 }
-// function convertToFahrenheit() {
-//     const celsius = getElementNumberValue("first")
-//     const convertToFahrenheit = celsius * 1.8 + 32
-//     writeResult(convertToFahrenheit)
-// }
-
-// function convertToCelsius() {
-//     const fahrenheit = getElementNumberValue("second")
-//     const convertToCelsius = (fahrenheit - 32) * 5 / 9
-//     writeResult(convertToCelsius)
-// }
-
-
-
-// const atsakymas1 = getElementNumberValue("first")
-//     // console.log(atsakymas1)
-
-// const atsakymas2 = getElementNumberValue("second")
-//     // console.log(atsakymas2)
-
-
-
-
-// const resultParagraph = document.getElementById("result")
-// resultParagraph.innerHTML = "result is: " + result
