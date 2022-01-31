6ad794bec12c79708875add88e297d912825b278
T.Kav <tomas@plienas.lt>
2022-01-31 19:54:52 +0200

    lyginiai skaiciai, kai slayga nelygu, naudojant continue

--- 35.js
+++ 35.js
@@ -1,9 +1,14 @@
 function lyginiaiSkaiciai(iki5k) {
     for (let i = 2; i <= iki5k; i = i + 1) {
         const liekana = i % 2
-        if (liekana === 0) {
-            console.log(i)
+        if (liekana !== 0) {
+            continue;
         }
+
+        console.log(i)
+
+        // kai bus continue, sita dalis neveik
     }
 }
+
 lyginiaiSkaiciai(1500)
